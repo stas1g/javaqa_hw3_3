76b367cb157f2689f48182c710db85ad3b197618
Fantasma <kadr-90@mail.ru>
2025-02-02 23:12:01 +0700

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,9 @@
+public class CreditPaymentService {
+    public int calculate (double loanAmount, int loanTermMonths, double rate) {
+        double monthlyRate = rate / 12 / 100;
+        double annuityPayment = loanAmount * (monthlyRate * Math.pow(1 + monthlyRate, loanTermMonths))
+                / (Math.pow(1 + monthlyRate, loanTermMonths) - 1);
+        return (int) Math.round(annuityPayment);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        int payment1 = service.calculate(1_000_000, 12, 9.99);
+        System.out.println("Ежемесячный платеж (1 год): " + payment1 + " руб.");
+
+        int payment2 = service.calculate(1_000_000, 24, 9.99);
+        System.out.println("Ежемесячный платеж (2 год): " + payment2 + " руб.");
+
+        int payment3 = service.calculate(1_000_000, 36, 9.99);
+        System.out.println("Ежемесячный платеж (3 год): " + payment3 + " руб.");
+        }
+    }
